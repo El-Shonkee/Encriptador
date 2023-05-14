00ea687e42ce64df12ef8c2a282ec6c5e7022c2d
El-Shonkee <108562297+El-Shonkee@users.noreply.github.com>
2023-05-14 21:56:49 +0000

    agrega boton copiar, mejora de styles y animacion

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
 // Obtener referencias a los elementos del HTML con ayuda de los respectivos id's asignados
 const encriptarBtn = document.getElementById('encriptar');
 const desencriptarBtn = document.getElementById('desencriptar');
+const copiarBtn = document.getElementById('copiar-texto');
 let ingreso = document.getElementById("textoEntrada");
-let salida = document.getElementById("textoSalida")
+let salida = document.getElementById("textoSalida");
 
+
+//Constante donde se almacenan los datos a intercambiar, ufnciona en ambos sentidos
 const sustituciones ={'a':'ai',
                       'e':'enter',
                       'i':'imes',
                       'o':'ober',
                       'u':'ufat'
                       };
-
+//Esta funcion se encarga de poner la animación en la que los textarea giran 360 Grados
 function animarTexto(texto, elemento) {
     elemento.innerText = texto;
     elemento.style.display = "block";
-    elemento.style.animation = "desplazar 0.8s";
+    elemento.style.animation = "desplazar 1.5s";
 
     // Reinicia la animación después de completarse
     setTimeout(() => {
         elemento.style.animation = "";
-    }, 1000);
+    }, 1500);
 }
-
-
 
 // Función para encriptar un mensaje que recibe como parametro un mensaje tipo string
 function encriptar(mensaje) {
+    
     let mensajeEncriptado = ""; //creamos una variable tipo String
     for (let i = 0; i < mensaje.length; i++) {  // recorremos la longitud del texto que se ingreso al inicio
         let letra = mensaje.charAt(i); //se revisa letra por letra si existe en la constante susticiones
@@ -36,8 +38,10 @@ function encriptar(mensaje) {
         }
     }
     return mensajeEncriptado; // se devuelve el mensaje ya con los cambios
+    
 }
 
+//Función para desencriptar un mensaje que recibe como parametro un mensaje tipo string
 function desencriptar(mensajeEncriptado) {
     let mensajeDesencriptado = "";
     const invSustituciones = {};
@@ -66,20 +70,38 @@ function desencriptar(mensajeEncriptado) {
     return mensajeDesencriptado;
 }
 
-
-
 //evento de click al botón "Encriptar"
 encriptarBtn.addEventListener('click', () => {
     let mensaje = ingreso.value;
-    let mensajeEncriptado = encriptar(mensaje);
-    animarTexto(mensajeEncriptado, salida);
+    if (mensaje.trim().length === 0) {
+       alert("no se ingreso un mensaje");
+    } else {
+        let mensajeEncriptado = encriptar(mensaje);
+        animarTexto(mensajeEncriptado, salida);
+        animarTexto(ingreso, ingreso);
+    }
 });
 
 // evento de click al botón "Desencriptar"
 desencriptarBtn.addEventListener('click', () => {
-    let mensajeEncriptado = ingreso.value;
-    let mensajeDesencriptado = desencriptar(mensajeEncriptado);
-    animarTexto(mensajeDesencriptado,salida)
-
+    let mensaje = ingreso.value;
+    if (mensaje.trim().length === 0) {
+        alert("no se ingreso un mensaje");
+    } else {
+        let mensajeEncriptado = ingreso.value;
+        let mensajeDesencriptado = desencriptar(mensajeEncriptado);
+        animarTexto(mensajeDesencriptado,salida)
+        animarTexto(ingreso,ingreso);
+    }
 });
+
+copiarBtn.addEventListener('click', () => {
+    // Selecciona el contenido del textarea
+    salida.select();
+  
+    // Copia el contenido seleccionado al portapapeles
+    document.execCommand("copy");
+  });
+
+
 
